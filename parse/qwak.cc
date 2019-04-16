f9dc439a7e21f802fc63bbe8823b8edd65a70aac
Gabriel <XyPhoGR@gmail.com>
2019-04-15 20:38:41 -0500

    trying to figure out a parsing lib

--- parse/qwak.cc
+++ parse/qwak.cc
@@ -1,1 +1,58 @@
 #include "qwak.h"
+
+namespace Qwak {
+
+//helpers
+
+template<typename T>
+void parseSome(TokenStream& tokens, vector<T>& store) {
+  T tmp;
+
+  int pos = tokens.getPos();
+  while(tmp.parse(tokens)) {
+    store.push_back(tmp);
+    pos = tokens.getPos();
+  }
+  tokens.setPos(pos);
+}
+
+//semantic elements
+
+//TODO
+bool Statement::parse(TokenStream& tokens) {
+}
+
+//TODO
+bool FunctionPrototype::parse(TokenStream& tokens) {
+}
+
+//TODO
+string FunctionPrototype::getName() {
+}
+
+bool Function::parse(TokenStream& tokens) {
+  int pos = tokens.getPos();
+
+  if(proto.parse(tokens)) {
+    parseSome<Statement>(tokens, statements);
+    return true;
+  } else {
+    tokens.setPos(pos);
+    return false;
+  }
+}
+
+//TODO
+string Function::getName() {
+}
+
+bool Program::parse(TokenStream& tokens) {
+  parseSome<Function>(tokens, functions);
+  for(Function& f: functions) {
+    functionsByName[f.getName()] = &f;
+  }
+
+  return true;
+}
+
+} //end namespace
--- parse/qwak.h
+++ parse/qwak.h
@@ -3,7 +3,55 @@
 
 #include "parse.h"
 
+#include <string>
+#include <vector>
+#include <unordered_map>
+
 namespace Qwak {
+
+using std::string;
+using std::vector;
+using std::unordered_map;
+
+//a statement of some sort that goes in a function body
+class Statement {
+private:
+
+public:
+  bool parse(TokenStream& tokens);
+  //TODO: API
+};
+
+//the declaration of a function, e.g. "function example(U, x)"
+class FunctionPrototype {
+private:
+
+public:
+  bool parse(TokenStream& tokens);
+  //TODO: API
+  string getName();
+};
+
+//a function and its body
+class Function {
+private:
+  FunctionPrototype proto;
+  vector<Statement> statements;
+public:
+  bool parse(TokenStream& tokens);
+  //TODO: API
+  string getName();
+};
+
+class Program {
+private:
+  vector<Function> functions;
+  unordered_map<string, Function*> functionsByName;
+public:
+  bool parse(TokenStream& tokens);
+  //TODO: API
+  Function& getFunction(string name);
+};
 
 class QwakParser {
 private:
